b297ef6154b236fe4bc291772e3190544a574f09
Ross Lubinski <lubinski@Rosss-MacBook-Pro.local>
2018-07-28 11:28:37 -0500

    JSON parsing of articles

--- NewsML/NewsHelper.swift
+++ NewsML/NewsHelper.swift
@@ -14,7 +14,34 @@ class NewsHelper {
     func getArticles() {
         
         Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=7a99487bfc4f4defb6b84284b2a69f9f").responseJSON { (response) in
-            <#code#>
+            if let json = response.result.value as? [String:Any] {
+                if let jsonArticles = json["articles"] as? [[String:Any]] {
+                    var articles = [Article]()
+                    for jsonArticle in jsonArticles {
+                        guard let title = jsonArticle["title"] as? String,
+                        let urlToImage = jsonArticle["urlToImage"] as? String,
+                        let url = jsonArticle["url"] as? String,
+                        let description = jsonArticle["description"] as? String
+                            else {
+                                continue
+                        }
+                        let article = Article()
+                        article.title = title
+                        article.urlToImage = urlToImage
+                        article.url = url
+                        article.description = description
+                        articles.append(article)
+                    }
+                }
+            }
         }
     }
+}
+
+class Article {
+    var title = ""
+    var urlToImage = ""
+    var url = ""
+    var description = ""
+    var category = ""
 }
