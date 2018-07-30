eb228db2e572f62808fc2f36c1c72279221932ae
Ross Lubinski <lubinski@Rosss-MacBook-Pro.local>
2018-07-30 16:57:26 -0500

    Implement webView

--- NewsML/ArticleTableViewController.swift
+++ NewsML/ArticleTableViewController.swift
@@ -16,6 +16,13 @@ class ArticleTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getArticles()
+        
+        //Remove table lines
+        tableView.separatorStyle = .none
+    }
+    
+    func getArticles() {
         NewsHelper().getArticles { (articles) in
             self.articles = articles
             self.tableView.reloadData()
@@ -32,11 +39,12 @@ class ArticleTableViewController: UITableViewController {
             
             let article = articles[indexPath.row]
             cell.titleLabel.text = article.title
-            cell.categoryLabel.text = article.category
+            cell.categoryLabel.text = article.category.rawValue
+            cell.categoryLabel.backgroundColor = article.categoryColor
             
             //Use Kingfisher to set image
             let url = URL(string: article.urlToImage)
-            cell.articleImageView.kf.setImage(with: url)
+            cell.articleImageView.kf.setImage(with: url, placeholder: UIImage(named: "defaultImage"), options: nil, progressBlock: nil, completionHandler: nil)
             
             return cell
         }
@@ -45,6 +53,25 @@ class ArticleTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 260
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let article = articles[indexPath.row]
+        performSegue(withIdentifier: "goToURL", sender: article)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToURL" {
+            if let article = sender as? Article {
+                if let webVC = segue.destination as? ArticleWebViewController {
+                    webVC.article = article
+                }
+            }
+        }
+    }
+    
+    @IBAction func reloadTapped(_ sender: UIBarButtonItem) {
+        getArticles()
     }
 }
 
--- NewsML/NewsHelper.swift
+++ NewsML/NewsHelper.swift
@@ -12,9 +12,11 @@ import DocumentClassifier
 
 class NewsHelper {
     
+    let API_KEY = "Enter API Key Here"
+    
     func getArticles(returnArticles : @escaping ([Article]) -> Void) {
         
-        Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=7a99487bfc4f4defb6b84284b2a69f9f").responseJSON { (response) in
+        Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=\(API_KEY)").responseJSON { (response) in
             if let json = response.result.value as? [String:Any] {
                 if let jsonArticles = json["articles"] as? [[String:Any]] {
                     var articles = [Article]()
@@ -33,7 +35,23 @@ class NewsHelper {
                         article.description = description
                         //User coreML to determine article type
                         guard let classification = DocumentClassifier().classify(description) else { return }
-                        article.category = classification.prediction.category.rawValue
+                        switch (classification.prediction.category) {
+                        case .business:
+                            article.category = .business
+                            article.categoryColor = UIColor(red: 0.298, green: 0.882, blue: 0.949, alpha: 1.00)
+                        case .entertainment:
+                            article.category = .entertainment
+                            article.categoryColor = UIColor(red: 0.129, green: 0.788, blue: 0.588, alpha: 1.00)
+                        case .sports:
+                            article.category = .sports
+                            article.categoryColor = UIColor(red: 0.996, green: 0.847, blue: 0.325, alpha: 1.00)
+                        case .politics:
+                            article.category = .politics
+                            article.categoryColor = UIColor(red: 0.929, green: 0.667, blue: 0.169, alpha: 1.00)
+                        case .technology:
+                            article.category = .technology
+                            article.categoryColor = UIColor(red: 0.949, green: 0.396, blue: 0.220, alpha: 1.00)
+                        }
                         articles.append(article)
                     }
                     returnArticles(articles)
@@ -48,5 +66,14 @@ class Article {
     var urlToImage = ""
     var url = ""
     var description = ""
-    var category = ""
+    var category : NewsCategory = .business
+    var categoryColor = UIColor.red
+}
+
+enum NewsCategory : String {
+    case business = "Business"
+    case entertainment = "Entertainment"
+    case politics = "Politics"
+    case sports = "Sports"
+    case technology = "Technology"
 }
